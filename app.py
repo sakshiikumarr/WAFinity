ff55e1f2df474679b86515f636eacfc77fc367d9
Sakshi Kumar <sakshikumar2060@gmail.com>
2026-02-16 15:12:43 +0530

    Fixed Render port binding

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from src.hybrid_waf.routes.main import main_bp
-from src.hybrid_waf.routes.proxy import proxy_bp  # Import proxy Blueprint
+from src.hybrid_waf.routes.proxy import proxy_bp
+import os
 
 app = Flask(__name__)
 
-# Register blueprints
 app.register_blueprint(main_bp)
 app.register_blueprint(proxy_bp)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
